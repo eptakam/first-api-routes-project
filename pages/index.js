f0d444d85165f6faeb28456b2601e64dd7da5a85
eptakam <emmataks@gmail.com>
2024-07-25 08:32:48 -0300

    Preparing the frontend form.

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,46 @@
+/*
+    je desire communiquer avec le serveur mais je ne veux pas que le client puisse voir ce que je fais. c'est la que entre en jeu les API routes. 
+
+    j'ai besoin d'une reference a mes input (formulaire) qui va me permettre de recuperer les valeurs entrees par l'utilisateur. D'ou l'utilisation du hook useRef de react
+*/
+
+import { useRef } from 'react';
+
 function HomePage() {
+  const emailInputRef = useRef();
+  const feedbackInputRef = useRef();
+
+  // gestionnaire de la soumission du formulaire
+  function submitFormHandler(event) {
+    event.preventDefault(); // empeche le rechargement de la page a la soumission du formulaire
+
+    // recuperer l'email entre par l'utilisateur grace a la reference a l'input
+    const enteredEmail = emailInputRef.current.value;
+
+    // recuperer le feedback entre par l'utilisateur grace a la reference a l'input
+    const enteredFeedback = feedbackInputRef.current.value;
+
+    // valider les donnees entrees par l'utilisateur
+    if (enteredEmail.trim() === '' || !enteredEmail.trim().includes('@') || enteredFeedback.trim() === '') {
+      alert('Email and Feedback must not be empty!');
+      return;
+    }
+  }
+
   return (
     <div>
       <h1>The Home Page</h1>
+      <form onSubmit={submitFormHandler}>
+        <div>
+          <label htmlFor="email">Your Email Address</label>
+          <input type="email" id="email" name="email" ref={emailInputRef} />
+        </div>
+        <div>
+          <label htmlFor="feedback">Your Feedback</label>
+          <textarea id="feedback" name="feedback" rows='5' ref={feedbackInputRef}></textarea>
+        </div>
+        <button>Send Feedback</button>
+      </form>
     </div>
   );
 }
